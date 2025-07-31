de890c070748e782f5904233c88e86f7ed0d5284
snehalll06 <snehaljadhav3106@gmail.com>
2025-07-31 07:32:57 +0530

    day14

---
+++ Day14/Day14.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Day14{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the cost per hour for first N hours: ");
+        int R1=sc.nextInt();
+        System.out.println("Enter the number of first N hours: ");
+        int N=sc.nextInt();
+        System.out.println("Enter the cost per hour after N hours: ");
+        int R2=sc.nextInt();
+        System.out.println("Enter the total travel time in minutes: ");
+        int X=sc.nextInt();
+
+        int total=0;
+        total+=R1*N;
+        total+=(X-(N*60))*R2/(60);
+
+        System.out.println(total);
+        
+
+
+    }
+}
